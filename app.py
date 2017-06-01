76ad5d7ca3e9989b76b98d5feb260e19bae49583
litingyang <F74032023@mail.ncku.edu.tw>
2017-06-01 20:03:46 +0800

    final project

---
+++ app.py
@@ -0,0 +1,97 @@
+import sys
+from io import BytesIO
+import requests
+import telegram
+from flask import Flask, request, send_file
+
+from fsm import TocMachine
+
+
+API_TOKEN = '366610620:AAG8v-PsZF5JTeLS1crrRJn_PZ9hlQJ9zLA'
+WEBHOOK_URL = 'https://d2ad16f9.ngrok.io/hook'
+
+app = Flask(__name__)
+bot = telegram.Bot(token=API_TOKEN)
+machine = TocMachine(
+    states=[
+        'user',
+        'state1',
+        'state2',
+        'state3',
+        'state4',
+        'state5'
+    ],
+    transitions=[
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'state1',
+            'conditions': 'is_going_to_state1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'state2',
+            'conditions': 'is_going_to_state2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'state3',
+            'conditions': 'is_going_to_state3'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'state3',
+            'dest': 'state4',
+            'conditions': 'is_going_to_state4'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state5',
+            'conditions': 'is_going_to_state5'
+        },
+        {
+            'trigger': 'go_back',
+            'source': [
+                'state1',
+                'state5',
+                'state4'
+            ],
+            'dest': 'user'
+        }
+    ],
+    initial='user',
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+
+def _set_webhook():
+    status = bot.set_webhook(WEBHOOK_URL)
+    if not status:
+        print('Webhook setup failed')
+        sys.exit(1)
+    else:
+        print('Your webhook URL has been set to "{}"'.format(WEBHOOK_URL))
+
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    machine.advance(update)
+    return 'ok'
+
+
+@app.route('/show-fsm', methods=['GET'])
+def show_fsm():
+    byte_io = BytesIO()
+    machine.graph.draw(byte_io, prog='dot', format='png')
+    byte_io.seek(0)
+    return send_file(byte_io, attachment_filename='fsm.png', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    _set_webhook()
+    app.run()
---
+++ fsm.py
@@ -0,0 +1,144 @@
+
+import requests
+from transitions.extensions import GraphMachine
+import re
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(
+            model = self,
+            **machine_configs
+        )
+
+    def is_going_to_state1(self, update):
+        text = update.message.text
+        return text.lower() == 'help'
+
+    def is_going_to_state2(self, update):
+        text = update.message.text
+
+        return (text.lower() == '查詢車次'or text.lower() == '2')
+
+    def is_going_to_state3(self, update):
+        text = update.message.text
+        return (text.lower() == '列車時刻查詢'or text.lower() == '1')
+
+    def is_going_to_state4(self, update):
+        text = update.message.text
+        pattern = "起程站[:|：]\s*([\u4E00-\u9FFF]+)\s*到達站[:|：]\s*([\u4E00-\u9FFF]+)\s*日期[:|：]\s*(\d\d\d\d/\d\d/\d\d)\s*時間從(\d\d)[:|：](\d\d)至(\d\d)[:|：](\d\d)"
+        match = re.search(pattern,text)
+        self.message = text
+        if match:
+            return 1
+        else:
+            return 0
+
+    def is_going_to_state5(self, update):
+        text = update.message.text
+        pattern = "\s*日期[:|：]\s*(\d\d\d\d/\d\d/\d\d)\s*車次[:|：](\d+)"
+        match = re.search(pattern,text)
+        self.message = text
+        if match:
+            return 1
+        else:
+            return 0
+
+    def on_enter_state1(self, update):
+        string = "please enter: 1.列車時刻查詢 2.查詢車次"
+        update.message.reply_text(string)
+        self.go_back(update)
+
+    def on_exit_state1(self, update):
+        print('Leaving state1')
+
+    def on_enter_state2(self, update):
+        update.message.reply_text("請輸入\n日期(ex:2017/06/08):\n車次(ex:129):")
+        self.advance(update)
+
+    def on_exit_state2(self, update):
+        print('Leaving state2')
+
+    def on_enter_state3(self, update):
+        update.message.reply_text("請輸入\n起程站(ex:臺北):\n到達站(ex:高雄):\n日期:(ex:2017/06/08)\n時間從(ex:12:00)至(ex:23:59)")
+        self.advance(update)
+
+    def on_exit_state3(self, update):
+        print('Leaving state3')
+
+    def on_enter_state4(self, update):
+        input_text = self.message 
+        print(input_text)
+        pattern = "\s*起程站[:|：]\s*([\u4E00-\u9FFF]+)\s*到達站[:|：]\s*([\u4E00-\u9FFF]+)\s*日期[:|：]\s*(\d\d\d\d/\d\d/\d\d)\s*時間從(\d\d)[:|：](\d\d)至(\d\d)[:|：](\d\d)"
+        twrail = 'http://twtraffic.tra.gov.tw/twrail'
+        match = re.search(pattern,input_text)
+        fs = match.group(1)
+        ts = match.group(2)
+        day = match.group(3)
+        ft = match.group(4) + match.group(5)
+        tt = match.group(6) + match.group(7)
+        regular =  "TRStation.push\('(\d+)'\);TRStation.push\('(\d+)'\);"+"TRStation.push\('"+fs+"'\)"
+        req = requests.get(twrail)
+        match = re.search(regular,req.text)
+        string = "查無此資料" 
+        if match:
+            fromcity = match.group(1)
+            fromstation = match.group(2)
+            regular =  "TRStation.push\('(\d+)'\);TRStation.push\('(\d+)'\);"+"TRStation.push\('"+ts+"'\)"
+            match = re.search(regular,req.text)
+            if match:
+                tocity = match.group(1)
+                tostation = match.group(2)
+                payload = {'searchdate':day,'fromcity':fromcity,'tocity':tocity,'trainclass':'2','timetype':'1','fromstation':fromstation,'tostation':tostation,'fromtime':ft,'totime':tt}
+                url = 'http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0'
+                r = requests.get(url,params=payload)
+                train = "<span id=\"classname\">([\u4E00-\u9FFF]+)</span>"
+                number = "traincode=(\d+)"
+                time = "<td class=\"SearchResult_Time\" align=\"center\" width=\"65\"><font color=\"Black\">(\d\d:\d\d)</font></td><td class=\"SearchResult_Time\" align=\"center\" width=\"65\"><font color=\"Black\">(\d\d:\d\d)</font></td><td align=\"center\" width=\"80\"><font color=\"Black\">(\d\d小時\d\d分)</font></td>"
+                pay = "<span id=\"Label1\">(\$ \d+)</span>"
+                trainlist = re.findall(train,r.text)
+                numberlist = re.findall(number,r.text)
+                timelist = re.findall(time,r.text)
+                paylist = re.findall(pay,r.text)   
+                if len(trainlist)>0:
+                    string = ""
+                    for i in range(0,min(len(trainlist),100),1):
+                        string = string + "車種:" + trainlist[i] + " \n"
+                        string = string + "車次:" + numberlist[i] + " \n"
+                        string = string + fs + "開車時間:" + timelist[i][0] + " \n" + ts + "到達時間:" + timelist[i][1] + " \n" + "行駛時間:" + timelist[i][2]  + " \n"
+                        string = string + "票價:" + paylist[i] + " "   
+                        string = string + "\n\n" 
+
+
+        update.message.reply_text(string)
+        self.go_back(update)
+
+    def on_exit_state4(self, update):
+        print('Leaving state4')
+
+    def on_enter_state5(self, update):
+        input_text = self.message
+        print(input_text)
+        pattern = "\s*日期[:|：]\s*(\d\d\d\d/\d\d/\d\d)\s*車次[:|：](\d+)"
+        match = re.search(pattern,input_text)
+        string = "查無此資料" 
+        if match:
+            searchdate = match.group(1)
+            traincode = match.group(2)
+            url ="http://twtraffic.tra.gov.tw/twrail/mobile/TrainDetail.aspx?"
+            payload = {'searchdate':searchdate,'traincode':traincode}
+            r = requests.get(url,params=payload)
+            train = "TRSearchResult.push\('([\u4E00-\u9FFF]+)'\);TRSearchResult.push\('(\d\d)(\d\d)'\);TRSearchResult.push\('(\d\d)(\d\d)'\);"
+            trainlist = re.findall(train,r.text)
+            if len(trainlist)>0:
+                string = ""
+                for i in range(0,min(len(trainlist),100),1):
+                    string = string + "站名:" + trainlist[i][0] + " \n"
+                    string = string + "到達時間:" + trainlist[i][1] + ":" + trainlist[i][2] + " \n"
+                    string = string + "開車時間:" + trainlist[i][3] + ":" + trainlist[i][4] + " "
+                    string = string + "\n\n"  
+
+        update.message.reply_text(string)
+        self.go_back(update)
+
+    def on_exit_state5(self, update):
+        print('Leaving state5')
